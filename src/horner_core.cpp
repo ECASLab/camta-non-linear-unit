f738c3ec03b7d4681518c70704896d14076b5735
macho1997 <53346540+macho1997@users.noreply.github.com>
2026-04-12 12:02:39 -0600

    Refactor Horner's method for polynomial evaluation

--- src/horner_core.cpp
+++ src/horner_core.cpp
@@ -8,53 +8,31 @@ data_t horner_core(
     data_t a1,
     data_t a0)
 {
-#pragma HLS INLINE off
-    if (degree < 0)
+#pragma HLS INLINE
+
+    int deg = degree;
+    if (deg < 0)
     {
         return static_cast<data_t>(0);
     }
 
-    if (degree > 3)
+    if (deg > 3)
     {
-        degree = 3;
+        deg = 3;
     }
 
     data_t coeffs[4] = {a0, a1, a2, a3};
 #pragma HLS ARRAY_PARTITION variable=coeffs complete
 
-    bool initialized = false;
     acc_t acc = 0;
 
     for (int d = 3; d >= 0; --d)
     {
-#pragma HLS PIPELINE II=1
-        if (d > degree)
+#pragma HLS UNROLL
+        if (d <= deg)
         {
-            continue;
+            acc = acc * x + coeffs[d];
         }
-
-        data_t c = coeffs[d];
-        if (!initialized)
-        {
-            if (c != 0)
-            {
-                acc = c;
-                initialized = true;
-            }
-        }
-        else
-        {
-            acc = acc * x;
-            if (c != 0)
-            {
-                acc = acc + c;
-            }
-        }
-    }
-
-    if (!initialized)
-    {
-        return static_cast<data_t>(0);
     }
 
     return static_cast<data_t>(acc);
