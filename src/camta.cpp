45a6638fdbd2cc2693b89502f543d6f49f90eb2f
macho1997 <53346540+macho1997@users.noreply.github.com>
2026-04-12 12:09:16 -0600

    Refactor camta function to use word_t types

--- src/camta.cpp
+++ src/camta.cpp
@@ -2,8 +2,8 @@
 #include "horner_core.h"
 
 void camta(
-    const data_t x_in[],
-    data_t y_out[],
+    const word_t x_in[],
+    word_t y_out[],
     int n,
     data_t L,
     int deg_r0,
@@ -14,8 +14,8 @@ void camta(
     data_t a3_r2, data_t a2_r2, data_t a1_r2, data_t a0_r2
 )
 {
-#pragma HLS INTERFACE m_axi offset=slave port=x_in bundle=gmem0
-#pragma HLS INTERFACE m_axi offset=slave port=y_out bundle=gmem1
+#pragma HLS INTERFACE m_axi offset=slave port=x_in bundle=gmem0 depth=1024
+#pragma HLS INTERFACE m_axi offset=slave port=y_out bundle=gmem1 depth=1024
 #pragma HLS INTERFACE s_axilite port=x_in
 #pragma HLS INTERFACE s_axilite port=y_out
 #pragma HLS INTERFACE s_axilite port=n
@@ -38,6 +38,9 @@ void camta(
 #pragma HLS INTERFACE s_axilite port=return
 
 #pragma HLS STABLE variable=L
+#pragma HLS STABLE variable=deg_r0
+#pragma HLS STABLE variable=deg_r1
+#pragma HLS STABLE variable=deg_r2
 #pragma HLS STABLE variable=a3_r0
 #pragma HLS STABLE variable=a2_r0
 #pragma HLS STABLE variable=a1_r0
@@ -51,34 +54,54 @@ void camta(
 #pragma HLS STABLE variable=a1_r2
 #pragma HLS STABLE variable=a0_r2
 
-#pragma HLS STABLE variable=deg_r0
-#pragma HLS STABLE variable=deg_r1
-#pragma HLS STABLE variable=deg_r2
+    const int n_words = (n + CAMTA_LANES - 1) / CAMTA_LANES;
 
-    for (int i = 0; i < n; ++i)
+word_loop:
+    for (int w = 0; w < n_words; ++w)
     {
 #pragma HLS PIPELINE II=1
-#pragma HLS LOOP_TRIPCOUNT min=1 max=4096 avg=1024
-        data_t x = x_in[i];
-        data_t a3,a2,a1,a0;
-        int deg;
+#pragma HLS LOOP_TRIPCOUNT min=1 max=1024 avg=128
+        word_t in_word = x_in[w];
+        word_t out_word = 0;
 
-        if (x < -L)
+    lane_loop:
+        for (int lane = 0; lane < CAMTA_LANES; ++lane)
         {
-            a3=a3_r0; a2=a2_r0; a1=a1_r0; a0=a0_r0;
-            deg = deg_r0;
-        }
-        else if (x > L)
-        {
-            a3=a3_r2; a2=a2_r2; a1=a1_r2; a0=a0_r2;
-            deg = deg_r2;
-        }
-        else
-        {
-            a3=a3_r1; a2=a2_r1; a1=a1_r1; a0=a0_r1;
-            deg = deg_r1;
+#pragma HLS UNROLL
+            const int idx = w * CAMTA_LANES + lane;
+            data_t y = 0;
+
+            if (idx < n)
+            {
+                data_t x = camta_unpack_lane(in_word, lane);
+                data_t a3;
+                data_t a2;
+                data_t a1;
+                data_t a0;
+                int deg;
+
+                if (x < -L)
+                {
+                    a3 = a3_r0; a2 = a2_r0; a1 = a1_r0; a0 = a0_r0;
+                    deg = deg_r0;
+                }
+                else if (x > L)
+                {
+                    a3 = a3_r2; a2 = a2_r2; a1 = a1_r2; a0 = a0_r2;
+                    deg = deg_r2;
+                }
+                else
+                {
+                    a3 = a3_r1; a2 = a2_r1; a1 = a1_r1; a0 = a0_r1;
+                    deg = deg_r1;
+                }
+
+                y = horner_core(x, deg, a3, a2, a1, a0);
+            }
+
+            camta_pack_lane(out_word, lane, y);
         }
 
-        y_out[i] = horner_core(x,deg,a3,a2,a1,a0);
+        y_out[w] = out_word;
     }
 }
